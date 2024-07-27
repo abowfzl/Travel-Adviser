35ca403c8e2bd665c6fda23da088bce65695295f
Abolfazl Moslemian <abowfzl@gmail.com>
2024-07-27 20:47:13 +0330

    refactor gpt4all_embeddings.py

--- api/src/Utils/gpt4all_embeddings.py
+++ api/src/Utils/gpt4all_embeddings.py
@@ -1,32 +1,6 @@
 import csv
-import os
-
 import pandas as pd
-from neo4j import GraphDatabase
 from nomic import embed
-
-
-def get_movie_plots(limit=None):
-    driver = GraphDatabase.driver(
-        os.getenv('NEO4J_URI'),
-        auth=(os.getenv('NEO4J_USERNAME'), os.getenv('NEO4J_PASSWORD'))
-    )
-
-    driver.verify_connectivity()
-
-    query = """MATCH (m:Movie) WHERE m.plot IS NOT NULL
-    RETURN m.movieId AS movieId, m.title AS title, m.plot AS plot"""
-
-    if limit is not None:
-        query += f' LIMIT {limit}'
-
-    movies, summary, keys = driver.execute_query(
-        query
-    )
-
-    driver.close()
-
-    return movies
 
 
 def generate_embeddings(file_name, limit=None):
@@ -35,7 +9,7 @@ def generate_embeddings(file_name, limit=None):
     output_plot = csv.DictWriter(csvfile_out, fieldnames=fieldnames)
     output_plot.writeheader()
 
-    attractions_df = pd.read_csv('C:/Users/Abolfazl/OneDrive/Abolfazl/OneDrive/Desktop/شهر ها/attractions.csv')
+    attractions_df = pd.read_csv('C:/Users/Abolfazl/OneDrive/Abolfazl/OneDrive/Desktop/Cities/attractions.csv')
 
     attractions = attractions_df[['attractionId', 'city_name', 'name', 'title', 'text']]
 
@@ -57,4 +31,4 @@ def generate_embeddings(file_name, limit=None):
     csvfile_out.close()
 
 
-generate_embeddings('C:/Users/Abolfazl/OneDrive/Abolfazl/OneDrive/Desktop/شهر ها/attraction-embeddings.csv')
+generate_embeddings('C:/Users/Abolfazl/OneDrive/Abolfazl/OneDrive/Desktop/Cities/attraction-embeddings.csv')
